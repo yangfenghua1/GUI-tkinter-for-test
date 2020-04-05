1a879ce4cfcb1eeb5c3d7a4b03dc9a449d42b3d0
yangfenghua <yangfh_mail@163.com>
2020-04-05 23:39:44 +0800

    use from tkinter import *

--- cal.py
+++ cal.py
@@ -1,59 +1,59 @@
-import tkinter
+from tkinter import *
 import xlrd
 import os
 
 
-root = tkinter.Tk()
+root = Tk()
 root.title('NaCl-Calculation')
 
-frame = tkinter.Frame(root)
+frame = Frame(root)
 
 frame.pack(padx=130, pady=80)
 
-v1 = tkinter.StringVar()
-v2 = tkinter.StringVar()
-v3 = tkinter.StringVar()
-v4 = tkinter.StringVar()
-v5 = tkinter.StringVar()
-v6 = tkinter.StringVar()
-v7 = tkinter.StringVar()
-v8 = tkinter.StringVar()
-v9 = tkinter.StringVar()
-v10 = tkinter.StringVar()
+v1 = StringVar()
+v2 = StringVar()
+v3 = StringVar()
+v4 = StringVar()
+v5 = StringVar()
+v6 = StringVar()
+v7 = StringVar()
+v8 = StringVar()
+v9 = StringVar()
+v10 = StringVar()
 def test(content):
 
     return content.isdigit()
 
 
 testCMD = frame.register(test)
-tkinter.Label(frame, text='COD-setting:').grid(row=0, column=1)
-e1 = tkinter.Entry(frame, width=60, textvariable=v1, validate='key', \
+Label(frame, text='COD-setting:').grid(row=0, column=1)
+e1 = Entry(frame, width=60, textvariable=v1, validate='key', \
            validatecommand=(testCMD, '%P')).grid(row=0, column=2)
-tkinter.Label(frame, text='COD-result:').grid(row=0, column=4)
-e2 = tkinter.Entry(frame, width=60, textvariable=v6, state='readonly').grid(row=0, column=5)
+Label(frame, text='COD-result:').grid(row=0, column=4)
+e2 = Entry(frame, width=60, textvariable=v6, state='readonly').grid(row=0, column=5)
 
  
-tkinter.Label(frame, text='Si-setting:').grid(row=1, column=1)
-e3 = tkinter.Entry(frame, width=60, textvariable=v2, validate='key', \
+Label(frame, text='Si-setting:').grid(row=1, column=1)
+e3 = Entry(frame, width=60, textvariable=v2, validate='key', \
            validatecommand=(testCMD, '%P')).grid(row=1, column=2)
-tkinter.Label(frame, text='Si-result:').grid(row=1, column=4)
-e4 = tkinter.Entry(frame, width=60, textvariable=v7, state='readonly').grid(row=1, column=5)
+Label(frame, text='Si-result:').grid(row=1, column=4)
+e4 = Entry(frame, width=60, textvariable=v7, state='readonly').grid(row=1, column=5)
 
-tkinter.Label(frame, text='Co3-setting:').grid(row=2, column=1)
-e5 = tkinter.Entry(frame, width=60, textvariable=v3, validate='key', \
+Label(frame, text='Co3-setting:').grid(row=2, column=1)
+e5 = Entry(frame, width=60, textvariable=v3, validate='key', \
            validatecommand=(testCMD, '%P')).grid(row=2, column=2)
-tkinter.Label(frame, text='Co3-result:').grid(row=2, column=4)
-e6 = tkinter.Entry(frame, width=60, textvariable=v8, state='readonly').grid(row=2, column=5)
+Label(frame, text='Co3-result:').grid(row=2, column=4)
+e6 = Entry(frame, width=60, textvariable=v8, state='readonly').grid(row=2, column=5)
 
  
-tkinter.Label(frame, text='Na2So4-setting:').grid(row=3, column=1)
-e7 = tkinter.Entry(frame, width=60, textvariable=v4, validate='key', \
+Label(frame, text='Na2So4-setting:').grid(row=3, column=1)
+e7 = Entry(frame, width=60, textvariable=v4, validate='key', \
            validatecommand=(testCMD, '%P')).grid(row=3, column=2)
-tkinter.Label(frame, text='Na2So4-result:').grid(row=3, column=4)
-e8 = tkinter.Entry(frame, width=60, textvariable=v9, state='readonly').grid(row=3, column=5)
+Label(frame, text='Na2So4-result:').grid(row=3, column=4)
+e8 = Entry(frame, width=60, textvariable=v9, state='readonly').grid(row=3, column=5)
 
-tkinter.Label(frame, text='MinValue:').grid(row=9, column=4)
-e9 = tkinter.Entry(frame, width=60, textvariable=v10, state='readonly').grid(row=9, column=5)
+Label(frame, text='MinValue:').grid(row=9, column=4)
+e9 = Entry(frame, width=60, textvariable=v10, state='readonly').grid(row=9, column=5)
 
 
 def calc():
@@ -76,8 +76,8 @@ def calc():
 	v9.set(str(result_Na2So4))
 	v10.set(str(min(result_COD,result_SI,result_CO3,result_Na2So4)))
     
-tkinter.Button(frame, text='result', command=calc, width=20,bg = 'green').grid(row=8, column=4, pady=5)
+Button(frame, text='result', command=calc, width=20,bg = 'green').grid(row=8, column=4, pady=5)
 
 
 
-tkinter.mainloop()
+mainloop()
